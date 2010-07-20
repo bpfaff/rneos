5e26f30be50a5bbf88232f27f7d8c08fae1e570b
Bernhard Pfaff <bernhard@pfaffikus.de>
2010-07-20 18:19:58 +0000

    Added class for answers from NEOS

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -2,4 +2,7 @@
 ## Class for NEOS communication objects
 ##
 setClass("NeosComm", representation(url = "character", curlopts = "list", curlhandle = "CURLHandle"))
-
+##
+## Class for returned Values from requests to NEOS 
+##
+setClass("NeosAns", representation(ans = "character", method = "character", call = "call", nc = "NeosComm"))
