df25b2183d7398322c9cbff70e46474cfcee3776
Bernhard Pfaff <bernhard@pfaffikus.de>
2010-08-12 11:47:27 +0000

    Added function for listCategories

---
+++ R/NlistCategories.R
@@ -0,0 +1,12 @@
+##
+## XML-RPC method: listCategories()
+##
+NlistCategories <- function(convert = TRUE, nc = CreateNeosComm()){
+  if(!(class(nc) == "NeosComm")){
+    stop("\nObject provided for 'nc' must be of class 'NeosComm'.\n")
+  }
+  call <- match.call()
+  ans <- xml.rpc(url = nc@url, method = "listCategories", .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
+  res <- new("NeosAns", ans = ans, method = "listCategories", call = call, nc = nc)
+  return(res)
+}
