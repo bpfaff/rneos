e9008f102e5d9de4cdd719c76fbfc039a5ccb892
Bernhard Pfaff <bernhard@pfaffikus.de>
2010-08-13 11:22:48 +0000

    Made check of category compulsory

--- R/NlistSolversInCategory.R
+++ R/NlistSolversInCategory.R
@@ -1,18 +1,16 @@
 ##
 ## XML-RPC method: listSolversInCategory()
 ##
-NlistSolversInCategory <- function(category, convert = TRUE, nc = CreateNeosComm(), checkcategory = FALSE) 
+NlistSolversInCategory <- function(category, convert = TRUE, nc = CreateNeosComm()) 
 {
     if (!(class(nc) == "NeosComm")) {
         stop("\nObject provided for 'nc' must be of class 'NeosComm'.\n")
     }
     category <-  as.character(category)
-    if(checkcategory){
-      cats <- NlistCategories()@ans
-      cats <- unlist(lapply(strsplit(cats, split = ":"), function(x) x[1]))
-      if(!category %in% cats){
-        stop(paste("\nSpecified category not available. Must be one of:\n", paste(cats, collapse = ", "), ".\n"))
-      }
+    cats <- NlistCategories(nc = nc)@ans
+    cats <- unlist(lapply(strsplit(cats, split = ":"), function(x) x[1]))
+    if(!category %in% cats){
+      stop(paste("\nSpecified category not available. Must be one of:\n", paste(cats, collapse = ", "), ".\n"))
     }
     call <- match.call()
     ans <- xml.rpc(url = nc@url, method = "listSolversInCategory", .args = list(category = category), .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
