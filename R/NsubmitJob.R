64b055b9352882debb30f75d0e610f40f88c483d
Bernhard Pfaff <bernhard@pfaffikus.de>
2010-08-25 12:17:30 +0000

    Included function for submitting jobs to Neos

---
+++ R/NsubmitJob.R
@@ -0,0 +1,12 @@
+##
+## XML-RPC method: submitJob()
+##
+NsubmitJob <- function(xmlstring, user = "rneos", interface = "R", id = 0, nc = CreateNeosComm()){
+  if(!(class(nc) == "NeosComm")) {
+    stop("\nObject provided for 'nc' must be of class 'NeosComm'.\n")
+  }
+  call <- match.call()
+  ans <- xml.rpc(url = nc@url, method = "submitJob", .args = list(xmlstring = xmlstring, user = user, interface = interface, id = id), .convert = TRUE, .opts = nc@curlopts, .curl = nc@curlhandle)
+  res <- new("NeosJob", jobnumber = ans[[1]], password = ans[[2]], method = "submitJob", call = call, nc = nc)
+    return(res)
+}
