036423e6aa3695d63eee35ec1350dcade469ff6d
Bernhard Pfaff <bernhard@pfaffikus.de>
2010-09-17 11:18:13 +0000

    Included class NeosOff

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -14,5 +14,12 @@ setClass("NeosXml", representation(xml = "XMLNode", method = "character", call =
 ## Class for assigned jobnumber and password from NEOS 
 ##
 setClass("NeosJob", representation(jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
-
+##
+## Class for assigned jobnumber and password from NEOS 
+##
+setClass("NeosJob", representation(jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
+##
+## Class for NEOS offset 
+##
+setClass("NeosOff", representation(ans = "character", offset = "integer", jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
 
