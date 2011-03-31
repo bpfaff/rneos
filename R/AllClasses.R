21a01a24df39bd3620d3f7ad0f7fabce44081978
Bernhard Pfaff <bernhard@pfaffikus.de>
2011-03-31 11:11:53 +0000

    Removed class for Neos offset

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -18,8 +18,4 @@ setClass("NeosJob", representation(jobnumber = "numeric", password = "character"
 ## Class for assigned jobnumber and password from NEOS 
 ##
 setClass("NeosJob", representation(jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
-##
-## Class for NEOS offset 
-##
-setClass("NeosOff", representation(ans = "character", offset = "integer", jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
 
