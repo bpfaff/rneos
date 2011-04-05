0f6df24e9747ab16363d4afd075ca1bec0be0d52
Bernhard Pfaff <bernhard@pfaffikus.de>
2011-04-05 11:09:35 +0000

    Removed double entry for NeosJob

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -15,10 +15,6 @@ setClass("NeosXml", representation(xml = "XMLNode", method = "character", call =
 ##
 setClass("NeosJob", representation(jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
 ##
-## Class for assigned jobnumber and password from NEOS 
-##
-setClass("NeosJob", representation(jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
-##
 ## Class for NEOS offset 
 ##
 setClass("NeosOff", representation(ans = "character", offset = "integer", jobnumber = "numeric", password = "character", method = "character", call = "call", nc = "NeosComm"))
