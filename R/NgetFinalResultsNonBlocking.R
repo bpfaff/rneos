989a174eef1ba948ddcb8e41c9b4548b12b30b85
Bernhard Pfaff <bernhard@pfaffikus.de>
2011-04-07 11:51:40 +0000

    Added method getFinalResultsNonBlocking

---
+++ R/NgetFinalResultsNonBlocking.R
@@ -0,0 +1,21 @@
+##
+## XML-RPC method: getFinalResultsNonBlocking()
+##
+NgetFinalResultsNonBlocking <- function(obj, convert = TRUE){
+  if (!(class(obj) == "NeosJob")) {
+    stop("\nObject 'obj' is not of class 'NeosJob'.\n")
+  }
+  call <- match.call()  
+  jobnumber <- obj@jobnumber
+  password <- obj@password
+  nc <- obj@nc
+  ans <- xml.rpc(url = nc@url, method = "getFinalResultsNonBlocking", .args = list(jobnumber = jobnumber, password = password), .convert = FALSE, .opts = nc@curlopts, .curl = nc@curlhandle)
+  if(convert){
+    tmp <- xmlValue(xmlRoot(xmlParse(ans)))
+    tmp <- gsub("\\n", "", tmp)
+    class(tmp) <- "base64"
+    ans <- base64(tmp)
+  }
+  res <- new("NeosAns", ans = ans, method = "getFinalResultsNonBlocking", call = call, nc = nc)
+  return(res)
+}
