e8703ca30d9cea8506544ed694c6dae8e80a7184
Bernhard Pfaff <bernhard@pfaffikus.de>
2011-04-07 11:44:08 +0000

    included killmsg argument

--- R/NkillJob.R
+++ R/NkillJob.R
@@ -1,7 +1,7 @@
 ##
 ## XML-RPC method: killJob()
 ##
-NkillJob <- function(obj, convert = TRUE){
+NkillJob <- function(obj, killmsg = NULL, convert = TRUE){
   if(!(class(obj) == "NeosJob")){
     stop("\nObject 'obj' is not of class 'NeosJob'.\n")
   }
@@ -9,7 +9,12 @@ NkillJob <- function(obj, convert = TRUE){
   jobnumber <- obj@jobnumber
   password <- obj@password
   nc <- obj@nc
-  ans <- xml.rpc(url = nc@url, method = "killJob", .args = list(jobnumber = jobnumber, password = password), .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
+  if(is.null(killmsg)){
+    killmsg <- ""
+  } else {
+    killmsg <- as.character(killmsg)
+  }
+  ans <- xml.rpc(url = nc@url, method = "killJob", .args = list(jobnumber = jobnumber, password = password, killmsg = killmsg), .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
   res <- new("NeosAns", ans = ans, method = "killJob", call = call, nc = nc)
   return(res)
 }
