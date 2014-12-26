95785341adf46d61313b3f81aadbec601c605cbf
Bernhard Pfaff <bernhard@pfaffikus.de>
2014-12-26 13:35:41 +0000

    Introduced requireNamespace("XMLRPC", quietly = TRUE)

--- R/NlistCategories.R
+++ R/NlistCategories.R
@@ -2,14 +2,19 @@
 ## XML-RPC method: listCategories()
 ##
 NlistCategories <- function(convert = TRUE, nc = CreateNeosComm()){
-  if(!(class(nc) == "NeosComm")){
-    stop("\nObject provided for 'nc' must be of class 'NeosComm'.\n")
-  }
-  call <- match.call()
-  ans <- xml.rpc(url = nc@url, method = "listCategories", .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
-  if(convert){
-    ans <- paste(names(ans), as.character(ans), sep = ": ")
-  }
-  res <- new("NeosAns", ans = ans, method = "listCategories", call = call, nc = nc)
-  return(res)
+    if(requireNamespace("XMLRPC", quietly = TRUE)) {
+        if(!(class(nc) == "NeosComm")){
+            stop("\nObject provided for 'nc' must be of class 'NeosComm'.\n")
+        }
+        call <- match.call()
+        ans <- XMLRPC::xml.rpc(url = nc@url, method = "listCategories",
+                               .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
+        if(convert){
+            ans <- paste(names(ans), as.character(ans), sep = ": ")
+        }
+        res <- new("NeosAns", ans = ans, method = "listCategories", call = call, nc = nc)
+        return(res)
+    } else {
+        stop("Package 'XMLRPC' not available, please install first from Omegahat.")
+    }
 }
