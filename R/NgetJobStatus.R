191cf04045a12bbe3fe8425ae35284cbdce8ec9d
Bernhard Pfaff <bernhard@pfaffikus.de>
2014-12-26 13:35:04 +0000

    Introduced requireNamespace("XMLRPC", quietly = TRUE)

--- R/NgetJobStatus.R
+++ R/NgetJobStatus.R
@@ -2,14 +2,20 @@
 ## XML-RPC method: getJobStatus()
 ##
 NgetJobStatus <- function(obj, convert = TRUE){
-  if(!(class(obj) == "NeosJob")){
-    stop("\nObject 'obj' is not of class 'NeosJob'.\n")
-  }
-  call <- match.call()
-  jobnumber <- obj@jobnumber
-  password <- obj@password
-  nc <- obj@nc
-  ans <- xml.rpc(url = nc@url, method = "getJobStatus", .args = list(jobnumber = jobnumber, password = password), .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
-  res <- new("NeosAns", ans = ans, method = "getJobStatus", call = call, nc = nc)
-  return(res)
+    if(requireNamespace("XMLRPC", quietly = TRUE)) {
+        if(!(class(obj) == "NeosJob")){
+            stop("\nObject 'obj' is not of class 'NeosJob'.\n")
+        }
+        call <- match.call()
+        jobnumber <- obj@jobnumber
+        password <- obj@password
+        nc <- obj@nc
+        ans <- XMLRPC::xml.rpc(url = nc@url, method = "getJobStatus",
+                               .args = list(jobnumber = jobnumber, password = password),
+                               .convert = convert, .opts = nc@curlopts, .curl = nc@curlhandle)
+        res <- new("NeosAns", ans = ans, method = "getJobStatus", call = call, nc = nc)
+        return(res)
+    } else {
+        stop("Package 'XMLRPC' not available, please install first from Omegahat.")
+    }
 }
